8bc36c954647e6cd1e7d761c01c02af6c301ead4
Luca Realdi <luca.realdi@opencontent.it>
2025-02-07 01:11:05 +0100

    Add eng-GB@euro

---
+++ translations/eng-GB@euro/translation.ts
@@ -0,0 +1,162 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+  <context>
+    <name>extension/ocmultibinary</name>
+    <message>
+      <source>Select files</source>
+      <translation>Select file</translation>
+    </message>
+    <message>
+      <source>Filename</source>
+      <translation>File name</translation>
+    </message>
+    <message>
+      <source>Status</source>
+      <translation>Status</translation>
+    </message>
+    <message>
+      <source>Display name</source>
+      <translation>Displayed title</translation>
+    </message>
+    <message>
+      <source>Display group</source>
+      <translation>Grouping</translation>
+    </message>
+    <message>
+      <source>Text</source>
+      <translation>Description</translation>
+    </message>
+    <message>
+      <source>Size</source>
+      <translation>Dimension</translation>
+    </message>
+    <message>
+      <source>Drag file here.</source>
+      <translation>Drag file here</translation>
+    </message>
+    <message>
+      <source>Drag files here.</source>
+      <translation>Drag files here</translation>
+    </message>
+    <message>
+      <source>A file with this name is already exists.</source>
+      <translation>A file with the same name already exists</translation>
+    </message>
+    <message>
+      <source>You are allowed to upload only one file.</source>
+      <translation>You can only upload one file</translation>
+    </message>
+    <message>
+      <source>You are allowed to upload only %s files.</source>
+      <translation>You can only upload %s files</translation>
+    </message>
+    <message>
+      <source>Attached files:</source>
+      <translation>Files uploaded:</translation>
+    </message>
+    <message>
+      <source>No files uploaded</source>
+      <translation>No file uploaded</translation>
+    </message>
+    <message>
+      <source>Delete all files</source>
+      <translation>Delete all files</translation>
+    </message>
+    <message>
+      <source>Remove this file</source>
+      <translation>Remove this file</translation>
+    </message>
+    <message>
+      <source>Add file</source>
+      <translation>Add files</translation>
+    </message>
+    <message>
+      <source>Can not create local file. Please contact the site administrator.</source>
+      <translation>Failed to create local file. Please contact your website administrator.</translation>
+    </message>
+    <message>
+      <source>Original file not found. Please contact the site administrator.</source>
+      <translation>Original file not found. Please contact your website administrator.</translation>
+    </message>
+    <message>
+      <source>The uploaded file exceeds the upload_max_filesize directive in php.ini</source>
+      <translation>File size is too big</translation>
+    </message>
+    <message>
+      <source>The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form</source>
+      <translation>File size is too big</translation>
+    </message>
+    <message>
+      <source>The uploaded file was only partially uploaded</source>
+      <translation>The file was only partially uploaded</translation>
+    </message>
+    <message>
+      <source>No file was uploaded</source>
+      <translation>No file was uploaded</translation>
+    </message>
+    <message>
+      <source>Missing a temporary folder</source>
+      <translation>Missing a temporary folder</translation>
+    </message>
+    <message>
+      <source>Failed to write file to disk</source>
+      <translation>Failed to write file to disk</translation>
+    </message>
+    <message>
+      <source>A PHP extension stopped the file upload</source>
+      <translation>System extension blocked file upload</translation>
+    </message>
+    <message>
+      <source>The uploaded file exceeds the post_max_size directive in php.ini</source>
+      <translation>The uploaded file exceeds the post_max_size directive</translation>
+    </message>
+    <message>
+      <source>File is too big</source>
+      <translation>File size is too big</translation>
+    </message>
+    <message>
+      <source>File is too small</source>
+      <translation>File size is too small</translation>
+    </message>
+    <message>
+      <source>Filetype not allowed</source>
+      <translation>File type not allowed</translation>
+    </message>
+    <message>
+      <source>Maximum number of files exceeded</source>
+      <translation>Maximum number of allowed files exceeded</translation>
+    </message>
+    <message>
+      <source>Image exceeds maximum width</source>
+      <translation>Image exceeds maximum width</translation>
+    </message>
+    <message>
+      <source>Image requires a minimum width</source>
+      <translation>Image requires a minimum width</translation>
+    </message>
+    <message>
+      <source>Image exceeds maximum height</source>
+      <translation>Image exceeds maximum height</translation>
+    </message>
+    <message>
+      <source>Image requires a minimum height</source>
+      <translation>Image requires a minimum height</translation>
+    </message>
+    <message>
+      <source>File upload aborted</source>
+      <translation>File upload stopped</translation>
+    </message>
+    <message>
+      <source>Failed to resize image</source>
+      <translation>Failed to resize image</translation>
+    </message>
+  </context>
+  <context>
+    <name>kernel/classes/datatypes</name>
+    <message>
+      <source>Multiple Files</source>
+      <translation>Multiple files</translation>
+    </message>
+  </context>
+</TS>
