347f1ea8e4ea4f6665dcd960322f30e3cd456297
Luca Realdi <luca.realdi@opencontent.it>
2025-06-03 11:40:35 +0200

    Add fre-FR translations

---
+++ translations/fre-FR/translation.ts
@@ -0,0 +1,162 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+  <context>
+    <name>extension/ocmultibinary</name>
+    <message>
+      <source>Select files</source>
+      <translation>Sélectionner un fichier</translation>
+    </message>
+    <message>
+      <source>Filename</source>
+      <translation>Nom du fichier</translation>
+    </message>
+    <message>
+      <source>Status</source>
+      <translation>État</translation>
+    </message>
+    <message>
+      <source>Display name</source>
+      <translation>Titre affiché</translation>
+    </message>
+    <message>
+      <source>Display group</source>
+      <translation>Regroupement</translation>
+    </message>
+    <message>
+      <source>Text</source>
+      <translation>Description</translation>
+    </message>
+    <message>
+      <source>Size</source>
+      <translation>Taille</translation>
+    </message>
+    <message>
+      <source>Drag file here.</source>
+      <translation>Faire glisser le fichier ici</translation>
+    </message>
+    <message>
+      <source>Drag files here.</source>
+      <translation>Faire glisser les fichiers ici</translation>
+    </message>
+    <message>
+      <source>A file with this name is already exists.</source>
+      <translation>Il existe déjà un fichier avec le même nom</translation>
+    </message>
+    <message>
+      <source>You are allowed to upload only one file.</source>
+      <translation>Vous pouvez télécharger un seul fichier</translation>
+    </message>
+    <message>
+      <source>You are allowed to upload only %s files.</source>
+      <translation>Vous pouvez télécharger %s fichier(s) uniquement</translation>
+    </message>
+    <message>
+      <source>Attached files:</source>
+      <translation>Fichiers joints :</translation>
+    </message>
+    <message>
+      <source>No files uploaded</source>
+      <translation>Aucun fichier téléchargé</translation>
+    </message>
+    <message>
+      <source>Delete all files</source>
+      <translation>Supprimer tous les fichiers</translation>
+    </message>
+    <message>
+      <source>Remove this file</source>
+      <translation>Supprimer ce fichier</translation>
+    </message>
+    <message>
+      <source>Add file</source>
+      <translation>Ajouter un fichier</translation>
+    </message>
+    <message>
+      <source>Can not create local file. Please contact the site administrator.</source>
+      <translation>Impossible de créer le fichier local. Veuillez contacter l’administrateur du site.</translation>
+    </message>
+    <message>
+      <source>Original file not found. Please contact the site administrator.</source>
+      <translation>Fichier d’origine introuvable. Veuillez contacter l’administrateur du site.</translation>
+    </message>
+    <message>
+      <source>The uploaded file exceeds the upload_max_filesize directive in php.ini</source>
+      <translation>La taille du fichier est trop grande</translation>
+    </message>
+    <message>
+      <source>The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form</source>
+      <translation>La taille du fichier est trop grande</translation>
+    </message>
+    <message>
+      <source>The uploaded file was only partially uploaded</source>
+      <translation>Le fichier n’a été que partiellement téléchargé</translation>
+    </message>
+    <message>
+      <source>No file was uploaded</source>
+      <translation>Aucun fichier n’a été téléchargé</translation>
+    </message>
+    <message>
+      <source>Missing a temporary folder</source>
+      <translation>Un dossier temporaire est manquant</translation>
+    </message>
+    <message>
+      <source>Failed to write file to disk</source>
+      <translation>Impossible d’enregistrer des fichiers sur le disque</translation>
+    </message>
+    <message>
+      <source>A PHP extension stopped the file upload</source>
+      <translation>Une extension de système a bloqué le téléchargement des fichiers</translation>
+    </message>
+    <message>
+      <source>The uploaded file exceeds the post_max_size directive in php.ini</source>
+      <translation>Le fichier téléchargé dépasse la directive post_max_size</translation>
+    </message>
+    <message>
+      <source>File is too big</source>
+      <translation>La taille du fichier est trop grande</translation>
+    </message>
+    <message>
+      <source>File is too small</source>
+      <translation>La taille du fichier est trop petite</translation>
+    </message>
+    <message>
+      <source>Filetype not allowed</source>
+      <translation>Type de fichier non accepté</translation>
+    </message>
+    <message>
+      <source>Maximum number of files exceeded</source>
+      <translation>Le nombre maximum de fichiers autorisés est dépassé</translation>
+    </message>
+    <message>
+      <source>Image exceeds maximum width</source>
+      <translation>L’image dépasse la largeur maximale</translation>
+    </message>
+    <message>
+      <source>Image requires a minimum width</source>
+      <translation>L’image nécessite une largeur minimale</translation>
+    </message>
+    <message>
+      <source>Image exceeds maximum height</source>
+      <translation>L’image dépasse la hauteur maximale</translation>
+    </message>
+    <message>
+      <source>Image requires a minimum height</source>
+      <translation>L’image nécessite une hauteur minimale</translation>
+    </message>
+    <message>
+      <source>File upload aborted</source>
+      <translation>Téléchargement du fichier interrompu</translation>
+    </message>
+    <message>
+      <source>Failed to resize image</source>
+      <translation>Impossible de redimensionner l’image</translation>
+    </message>
+  </context>
+  <context>
+    <name>kernel/classes/datatypes</name>
+    <message>
+      <source>Multiple Files</source>
+      <translation>Fichiers multiples</translation>
+    </message>
+  </context>
+</TS>
